6badd1e4e421f21ca8e05c4f5b7079c9b2d33112
Jonatan <jonatanav255@hotmail.com>
2024-10-06 11:34:52 -0600

    html and ts

---
+++ src/main.ts
@@ -0,0 +1,64 @@
+import './style.css'
+
+const contentDiv = document.getElementById('content') as HTMLDivElement
+const viewUsersLink = document.getElementById(
+  'view-users-link'
+) as HTMLAnchorElement
+const addUsersLink = document.getElementById(
+  'add-user-link'
+) as HTMLAnchorElement
+
+// Sample user data
+let users = [
+  { username: 'user1', email: 'user1@example.com' },
+  { username: 'user2', email: 'user2@example.com' }
+]
+function displayUsers () {
+  const userListHtml = `<h2>Get All Users</h2>
+                <ul>
+                ${users
+                  .map(user => `<li>${user.username} ${user.email}</li>`)
+                  .join('')}
+                </ul>`
+  contentDiv.innerHTML = userListHtml
+}
+function displayAddUserForm () {
+  const addUserFormHtml = `
+                  <h2>Create Account</h2>
+                  <form id="add-user-form">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" required>
+                    <label for="email">Email</label>
+                    <input type="email" id="email" required>
+                    <button type="submit">Add User</button>
+                  </form>
+                `
+  contentDiv.innerHTML = addUserFormHtml
+
+  const addUserForm = document.getElementById(
+    'add-user-form'
+  ) as HTMLFormElement
+  const userNameInput = document.getElementById('username') as HTMLInputElement
+  const emailInput = document.getElementById('email') as HTMLInputElement
+
+  addUserForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    users.push({ username: userNameInput.value, email: emailInput.value })
+
+    addUserForm.reset()
+
+    alert('User added successfully!')
+    displayUsers()
+  })
+}
+
+viewUsersLink.addEventListener('click', event => {
+  event.preventDefault()
+  displayUsers()
+})
+
+addUsersLink.addEventListener('click', event => {
+  event.preventDefault()
+  displayAddUserForm()
+})
